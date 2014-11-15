f4547e4b09d17ec41b289636225c31dba8ac8bea
Flaviu Tamas <tamasflaviu@gmail.com>
2014-11-15 14:28:01 -0500

    Add status icons

--- config.py
+++ config.py
@@ -20,6 +20,65 @@
 #
 #  - 'hide_release_builds': Whether to hide release builds. Defaults to false.
 
+import os
+from buildbot.status import html
+try:
+    # buildbot 0.8.7p1
+    from buildbot.status.results import SUCCESS, WARNINGS, FAILURE, SKIPPED, EXCEPTION
+    from buildbot.status.results import Results
+except ImportError:
+    # buildbot 0.8.0
+    from buildbot.status.builder import SUCCESS, WARNINGS, FAILURE, EXCEPTION, RETRY
+    from buildbot.status.builder import Results
+from buildbot.status.web.base import HtmlResource
+
+class BuildStatusImageResource(HtmlResource):
+    contentType = "image/svg+xml"
+
+    def __init__(self, categories=None):
+        HtmlResource.__init__(self)
+
+    def content(self, request, ctx):
+        """Display a build status image like Travis does."""
+
+        status = self.getStatus(request)
+        request.setHeader('Cache-Control', 'no-cache')
+
+        # Get the parameters.
+        name = request.args.get("builder", [None])[0]
+        number = request.args.get("number", [None])[0]
+        if not name or not number:
+            return "builder and number parameter missing"
+        number = int(number)
+
+        # Check if the builder in parameter exists.
+        try:
+            builder = status.getBuilder(name)
+        except:
+            return "unknown builder"
+
+        # Check if the build in parameter exists.
+        build = builder.getBuild(int(number))
+        if not build:
+            return "unknown build %s" % number
+
+        #SUCCESS, WARNINGS, FAILURE, SKIPPED or EXCEPTION
+        res = build.getResults()
+        resname = Results[res]
+
+        img = 'status-img/status_image_%s.svg' % resname
+        here = os.path.dirname(__file__)
+        imgfile = os.path.join(here, img)
+
+        imgcontent = open(imgfile, 'rb').read()
+
+        return imgcontent
+
+class WebStatus(html.WebStatus):
+    def setupUsualPages(self, numbuilds, num_events, num_events_max):
+        html.WebStatus.setupUsualPages(self, numbuilds, num_events, num_events_max)
+        self.putChild("buildstatusimage", BuildStatusImageResource())
+
 # Global Configuration
 from build_steps import construct_nim_build, python_exe_property_name
 
@@ -232,7 +291,7 @@ authz_cfg = authz.Authz(
     stopAllBuilds='auth',
     cancelPendingBuild='auth',
 )
-c['status'].append(html.WebStatus(http_port=8010, authz=authz_cfg))
+c['status'].append(WebStatus(http_port=8010, authz=authz_cfg))
 
 
 # PROJECT IDENTITY
