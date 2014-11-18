d28dfe6ed709d21fc603fff77d53a80c09503fa3
Varriount <Varriount@users.noreply.github.com>
2014-11-18 03:17:48 -0500

    Update build_steps.py

--- build_steps.py
+++ build_steps.py
@@ -374,7 +374,7 @@ def run_testament(platform):
             workdir=str(platform.nim_dir),
             env=base_env,
             haltOnFailure=True,
-            timeout=10800
+            timeout=21600
         )
     ]
 
